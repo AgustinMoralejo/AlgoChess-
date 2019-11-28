44d637769e3d9889f0e7c99dcbbb88bbd58869a6
AgustinMoralejo <amoralejo@fi.uba.ar>
2019-11-28 00:33:23 -0300

    archivos imagenes, falta alianzas

--- src/main/java/fiuba/algo3/TP2/Modelo/Excepciones/UnidadNoMovibleException.java
+++
@@ -1,4 +0,0 @@
-package fiuba.algo3.TP2.Modelo.Excepciones;
-
-public class UnidadNoMovibleException extends RuntimeException {
-}
--- src/main/java/fiuba/algo3/TP2/Modelo/Observer.java
+++
@@ -1,6 +0,0 @@
-package fiuba.algo3.TP2.Modelo;
-
-public interface Observer {
-    void actualizar();
-
-}
