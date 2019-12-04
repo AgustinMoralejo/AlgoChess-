7bdf92dfece2fd1cc34d742b3a8047c1dedc9a68
Agustin Leguizamon <agustinmleguizamon@gmail.com>
2019-12-04 16:40:22 -0300

    + Terreno finalizado

--- src/main/java/fiuba/algo3/TP2/Vista/Artista.java
+++ src/main/java/fiuba/algo3/TP2/Vista/Artista.java
@@ -2,26 +2,33 @@ package fiuba.algo3.TP2.Vista;
 
 import javafx.scene.image.Image;
 import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Rectangle;
 
 import java.util.Random;
+
+import static fiuba.algo3.TP2.Vista.VistaTablero.ALTO_CASILLERO;
+import static fiuba.algo3.TP2.Vista.VistaTablero.ANCHO_CASILLERO;
 
 public class Artista {
 
     private Ventana ventana;
-    private final String PISO = "src/main/java/fiuba/algo3/TP2/Vista/Imagenes/piso.jpg";
-    private final String PASTO = "src/main/java/fiuba/algo3/TP2/Vista/Imagenes/pasto.jpg";
-    private final String TIERRA = "src/main/java/fiuba/algo3/TP2/Vista/Imagenes/tierra2.jpg";
-    private final String PARED = "src/main/java/fiuba/algo3/TP2/Vista/Imagenes/pared.jpg";
-    private final String ARENA = "src/main/java/fiuba/algo3/TP2/Vista/Imagenes/arena2.jpg";
+    private int columna;
+    private int fila;
+    private final String PISO = "src/main/java/fiuba/algo3/TP2/Vista/Imagenes/Terreno/piso.jpg";
+    private final String PASTO = "src/main/java/fiuba/algo3/TP2/Vista/Imagenes/Terreno/pasto3.png";
+    private final String TIERRA = "src/main/java/fiuba/algo3/TP2/Vista/Imagenes/Terreno/tierra2.jpg";
+    private final String PARED = "src/main/java/fiuba/algo3/TP2/Vista/Imagenes/Terreno/pared.jpg";
+    private final String PARED_ROJO = "src/main/java/fiuba/algo3/TP2/Vista/Imagenes/Terreno/paredRojo.jpg";
+    private final String PARED_NATURAL = "src/main/java/fiuba/algo3/TP2/Vista/Imagenes/Terreno/agua2.png";
+    private final String ARENA = "src/main/java/fiuba/algo3/TP2/Vista/Imagenes/Terreno/arena3.png";
 
     public Artista(Ventana ventana){
         this.ventana = ventana;
+        this.fila = ventana.getCasillero().getFila();
+        this.columna = ventana.getCasillero().getColumna();
     }
 
     public ImagePattern getImagePattern(Ventana ventana) {
-
-        int fila = ventana.getCasillero().getFila();
-        int columna = ventana.getCasillero().getColumna();
 
         String path = PASTO;
 
@@ -29,18 +36,46 @@ public class Artista {
         int num = random.nextInt(2);
 
         if(num == 0){
-            path = TIERRA;
+            path = ARENA;
         }
 
         if(columna > 7 && columna < 12){
             path = PISO;
         }
-        else if (columna == 0 || fila == 0 || columna == 19 || fila == 19){
+        if(columna == 0 || columna == 19){
+            path = PARED_NATURAL;
+        }
+        if (fila == 0){
             path = PARED;
+        }
+        if(fila == 19){
+            path = PARED_ROJO;
         }
 
         Image imagen = new Image ("file:" + path);
 
         return new ImagePattern(imagen);
+    }
+
+    public Rectangle getPlanta() {
+
+        Random random = new Random();
+        String path;
+
+        int numPlanta = random.nextInt(15);
+        Rectangle reactanguloPlanta = new Rectangle(ANCHO_CASILLERO, ALTO_CASILLERO);
+
+        if(fila != 0 && fila != 19 && columna != 0 && columna != 19 && (columna < 8 || columna > 12) ){
+            if(numPlanta > 5){
+                numPlanta = 0;
+            }
+            path = ("file:src/main/java/fiuba/algo3/TP2/Vista/Imagenes/Terreno/planta" + (numPlanta) + ".png");
+        } else{
+            path = ("file:src/main/java/fiuba/algo3/TP2/Vista/Imagenes/Terreno/planta0.png");
+        }
+        Image imagen = new Image(path);
+        reactanguloPlanta.setFill(new ImagePattern(imagen));
+        return reactanguloPlanta;
+
     }
 }
--- src/main/java/fiuba/algo3/TP2/Vista/Ventana.java
+++ src/main/java/fiuba/algo3/TP2/Vista/Ventana.java
@@ -26,48 +26,46 @@ public class Ventana extends StackPane implements Observer {
     private Rectangle rec;
     private Casillero casillero;
 
-    private Text textoPosicion;
+    private final double STROKE_DEFAULT = 0.03;
+    private final double STROKE_RESALTAR_UNIDAD = 0.07;
+    private final double STROKE_RESALTAR_CASILLERO = 0.06;
+
+    private boolean alianza;
 
     public Ventana(boolean alianza, Casillero casillero,
                    ControladorJuego controladorJuego) {
 
-        this.artista = new Artista(this);
         this.casillero = casillero;
+        this.artista = new Artista(this);
         this.rec = new Rectangle();
+        this.alianza = alianza;
+
+
+        Rectangle rectanguloPlanta = artista.getPlanta();
 
         casillero.agregarObserver(this);
-
-        textoPosicion = new Text("(" + casillero.getFila() + "," + casillero.getColumna() + ")");
-
-        textoPosicion.setFont(Font.font("Liberation Serif", FontWeight.EXTRA_BOLD, (float) ALTO_CASILLERO / 5.0));
-        textoPosicion.setFill(Color.BLACK);
-        textoPosicion.setVisible(false);
 
         ImagePattern imagePattern = this.artista.getImagePattern(this);
         rec.setFill(imagePattern);
 
-
-        rec.setStroke(Color.BLACK);
-        rec.setStrokeWidth(ANCHO_CASILLERO * 0.05);
+        rec.setStroke(alianza ? Color.DARKBLUE : Color.DARKRED);
+        rec.setStrokeWidth(ANCHO_CASILLERO * STROKE_DEFAULT);
 
         rec.setWidth(ANCHO_CASILLERO);
-        rec.setHeight(VistaTablero.ALTO_CASILLERO);
+        rec.setHeight(ALTO_CASILLERO);
         relocate(casillero.getColumna() * ANCHO_CASILLERO,
-                casillero.getFila() * VistaTablero.ALTO_CASILLERO);
+                casillero.getFila() * ALTO_CASILLERO);
 
         /*
         rec.setFill(alianza ? Color.DARKBLUE
                 : Color.DARKRED);
-*/
-        getChildren().add(rec);
+        */
+
+        getChildren().addAll(rec, rectanguloPlanta);
 
         ControladorVistaCasillero controlador = new ControladorVistaCasillero(this, controladorJuego);
 
         this.setOnMouseClicked(controlador);
-
-        this.setOnMouseEntered(event -> textoPosicion.setVisible(true));
-
-        this.setOnMouseExited(event -> textoPosicion.setVisible(false));
 
 
     }
@@ -75,21 +73,21 @@ public class Ventana extends StackPane implements Observer {
 
     public void resaltar() {
         rec.setStrokeType(StrokeType.INSIDE);
-        rec.setStrokeWidth(ANCHO_CASILLERO * 0.08);
+        rec.setStrokeWidth(ANCHO_CASILLERO * STROKE_RESALTAR_UNIDAD);
         rec.setStroke(Color.DARKGREEN);
     }
 
 
     public void resaltar(int overload) {
         rec.setStrokeType(StrokeType.INSIDE);
-        rec.setStrokeWidth(ANCHO_CASILLERO * 0.06);
+        rec.setStrokeWidth(ANCHO_CASILLERO * STROKE_RESALTAR_CASILLERO);
         rec.setStroke(Color.DARKORANGE);
     }
 
     public void desResaltar() {
         rec.setStrokeType(StrokeType.CENTERED);
-        rec.setStroke(Color.BLACK);
-        rec.setStrokeWidth(ANCHO_CASILLERO * 0.05);
+        rec.setStroke(alianza ? Color.DARKBLUE : Color.DARKRED);
+        rec.setStrokeWidth(ANCHO_CASILLERO * STROKE_DEFAULT);
     }
 
     @Override
